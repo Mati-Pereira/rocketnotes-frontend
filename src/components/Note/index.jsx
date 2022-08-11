3dd789a59aa3ec011189da9dcc101b46d95da5ac
Matheus <matheus-rodrigues37@live.com>
2022-08-10 23:14:46 -0300

    resolvido

--- src/components/Note/index.jsx
+++ src/components/Note/index.jsx
@@ -2,14 +2,15 @@ import { Container } from "./styles";
 import Tag from "../Tag";
 
 function Note({ data, ...rest }) {
+  console.log(data)
   return (
     <Container {...rest}>
       <h1>{data.title}</h1>
       {data.tags && (
         <footer>
-          {data.tags.map((tag) => {
-            <Tag key={tag.id} title={tag.name} />;
-          })}
+          {data.tags.map((tag) => (
+            <Tag key={tag.id} title={tag.name} />
+          ))}
         </footer>
       )}
     </Container>
