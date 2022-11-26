4be327b59b70fd023e61155f03da320b307665f6
Matheus <matheus-rodrigues37@live.com>
2022-11-26 11:35:23 -0300

    up

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <AuthProvider>
-        {console.log()}
         <Routes />
       </AuthProvider>
     </ThemeProvider>
